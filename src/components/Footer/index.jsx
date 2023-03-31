21c6962da06b0b930a3c67f5ad48be7a3c356025
maud-vernet <vernet.maud@orange.fr>
2023-03-31 10:41:57 +0200

    feat footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import logo from '../../assets/logo-footer.png'
+import './footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <img src={logo} alt="logo" />
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './style/index.css'
+import './style/vars.css'
 import RoutesList from './components/Router'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -11,6 +13,7 @@ root.render(
         <Router>
             <Header />
             <RoutesList />
+            <Footer />
         </Router>
     </React.StrictMode>
 )
