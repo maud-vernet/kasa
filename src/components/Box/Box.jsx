98afb8eee3f43662a2fe9b0472f8c7dd88b883e2
maud-vernet <vernet.maud@orange.fr>
2023-04-05 17:31:37 +0200

    feat collapsing system 1st version

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -4,18 +4,41 @@ import './Box.css'
 import openingIcon from '../../assets/vector.svg'
 
 function Boxes() {
+    const [isClosed, setIsOpen] = useState(true)
+
     const boxes = aproposDetails.map((apropos) => (
         <div key={apropos.id} className="box">
-            <div className="box-header">
-                <h2 className="box-title">{apropos.title}</h2>
-                <button className="box-opening-system">
-                    <img
-                        src={openingIcon}
-                        alt="ouvrir la boîte pour lire le détail"
-                    />
-                </button>
-            </div>
-            <div className="box-description">{apropos.description}</div>
+            {isClosed ? (
+                <div className="box-header">
+                    <h2 className="box-title">{apropos.title}</h2>
+                    <button
+                        className="box-opening-system"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <img
+                            src={openingIcon}
+                            alt="fermer la boîte pour cacher le détail"
+                        />
+                    </button>
+                </div>
+            ) : (
+                <div>
+                    <div className="box-header">
+                        <h2 className="box-title">{apropos.title}</h2>
+                        <button
+                            className="box-opening-system"
+                            onClick={() => setIsOpen(true)}
+                        >
+                            <img
+                                src={openingIcon}
+                                alt="ouvrir la boîte pour lire le détail"
+                                className="closed"
+                            />
+                        </button>
+                    </div>
+                    <div className="box-description">{apropos.description}</div>
+                </div>
+            )}
         </div>
     ))
     return <section className="main-section">{boxes}</section>
